68b543c993587e54a9e72d707c0b22c6967f5d07
Darryl Quinn <dman776@gmail.com>
2018-02-23 13:54:30 -0600

    added license blocks

--- mongo_kml.py
+++ mongo_kml.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python
+#
+# Part of AREDN -- Used for creating Amateur Radio Emergency Data Networks
+# Copyright (C) 2018 Darryl Quinn
+#  Additional contributors: Conrad Lara
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Additional Terms:
+#
+# Additional use restrictions exist on the AREDN(TM) trademark and logo.
+#   See AREDNLicense.txt for more info.
+#
+# Attributions to the AREDN Project must be retained in the source code.
+# If importing this code into a new or existing project attribution
+# to the AREDN project must be added to the source code.
+#
+# You must not misrepresent the origin of the material contained within.
+#
+# Modified versions must be modified to attribute to the original source
+# and be marked in reasonable ways as differentiate it from the original
+# version.
+#
 '''Generate a KML document of a tour based on rotating around locations.
 '''
 from pykml.factory import nsmap
